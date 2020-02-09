9eca1de65ca6c7410e53b63634ea6ac11fc4a0c2
inci-k <ikaratay@na.edu>
2020-02-09 09:07:22 -0600

    my first commit

--- src/secondpackage/A01Second.java
+++ src/secondpackage/A01Second.java
@@ -1,6 +1,6 @@
 package secondpackage;
 
-public class A1Second {
+public class A01Second {
     public static void  main(String[] args){
         System.out.println("Hello World");
     }
--- src/secondpackage/A9Receipt.java
+++ src/secondpackage/A9Receipt.java
@@ -10,5 +10,21 @@ public class A9Receipt {
         System.out.format("\t\titem2 \t:\t  $%.2f",item2);
 
 
+        /*
+         public static void main(String[] args) {
+        float 123 = 2.49f;
+        float 124 = 4.79f;
+        float 125 = 1.65f;
+        float 126 = 7.88f;
+        float 127 = 0.99f;
+        System.out.format("\t\t\ Item id \t\t quantity \t\t price \n");
+        System.out.format("\t \t----------------------------------\n");
+        System.out.format("\t\t123 \t1\t  $%.2f\n",123);
+        System.out.format("\t\t125 \t2\t  $%.2f",125);
+
+         */
+
+
+
     }
 }
