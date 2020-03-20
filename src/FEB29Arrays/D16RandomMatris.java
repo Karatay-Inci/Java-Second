4c47bdf8363428ef9876c89d30e667f5583aedea
inci-k <ikaratay@na.edu>
2020-03-20 01:39:08 -0500

    classwork MARCH 20

--- src/FEB29Arrays/D16RandomMatris.java
+++ src/FEB29Arrays/D16RandomMatris.java
@@ -1,18 +1,18 @@
 package FEB29Arrays;
 
 public class D16RandomMatris {
-    /*public static void main(String[] args) {
-        int arrayNumbers[][] = {{0,0,0,0,},{0,0,0,0,0},{0,0,0,0,0}};
+    public static void main(String[] args) {
+        int arrayNumbers[][] = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
         for (int i = 0; i < arrayNumbers.length; i++) {
-                for (int j = 0; j < arrayNumbers[i].length; i++) {
+                for (int j = 0; j < arrayNumbers[i].length; j++) {
                     arrayNumbers[i][j] = (int) (Math.random() * 10);
                 }
         }
         for (int numberArray[]:arrayNumbers) {
                 for (int number:numberArray) {
-                    System.out.println(number+"\t");
+                    System.out.print(number+"\t");
                 }
             System.out.println();
         }
-    }*/
+    }
 }
--- src/FEB9/B10NestedLoop.java
+++ src/FEB9/B10NestedLoop.java
@@ -4,7 +4,7 @@ public class B10NestedLoop {
     public static void main(String[] args) {
         for(int i=0;    i<5;    i++){
             for(int j=0;    j<6;    j++){
-                //System.out.print("Hello World ");
+                System.out.print("Hello World ");
                 System.out.println(" i : " +i+ "j : " +j );
             }
         }
