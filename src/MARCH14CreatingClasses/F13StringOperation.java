1554222612264e66e6f5241f3887f3808ac645e4
inci-k <ikaratay@na.edu>
2020-03-25 01:58:33 -0500

    classwork MARCH 24

--- src/MARCH14CreatingClasses/F13StringOperation.java
+++ src/MARCH14CreatingClasses/F13StringOperation.java
@@ -18,14 +18,17 @@ public class F13StringOperation {
     public String returnCapitalized(){
 //first concat first letter and convert that letter to upper
         String result ="";
-        result+=this.value.charAt(0);
-        result+=result.toUpperCase();
+        String firstLetter="";                  //this variable for converting to String
+        firstLetter+=this.value.charAt(0);      //concat first letter with first char
+        //System.out.println(result);
+        result+=result.toUpperCase();           //concat first letter (upper case) and result
+       // System.out.println(result);
         String letter="";
         for (int i=1;  i<this.value.length();  i++){
             if (this.value.charAt(i-1)==' '){
 //Check previous char if it is space first convert to String using concat and then conver to upper
                 letter+= this.value.charAt(i);      //converting String
-                letter+= letter.toUpperCase();      //converting upper
+                letter= letter.toUpperCase();      //converting upper
                 result+=letter;                     //concat to result
                 letter="";                          //reset for the second word
             }
@@ -35,6 +38,41 @@ public class F13StringOperation {
         }
         return result;
     }
+    public String [] returnString(){
+        String[] result = new String[this.value.length()];
+        String letter;
+        for (int i=0;  i<this.value.length();  i++){
+            letter = ""+ this.value.charAt(i);
+            result[i]=letter;
+            letter="";
+        }
+        return result;
+    }
+    public String[] returnSentence(){
+        this.value+=" ";
+        int numberOfSpaces=1;
+        for(int i=0;  i<this.value.length();  i++){
+            if(this.value.charAt(i)==' '){
+                numberOfSpaces++;
+            }
+        }
+        String words[] = new String[numberOfSpaces];
+        String word="";
+        int index =0;
+        for (int i=0;  i<this.value.length();  i++){
+            if (this.value.charAt(i)!=' '){
+                word+=this.value.charAt(i);
+            }
+            else{
+                words[index]=word;
+                index++;
+                word="";
+            }
+        }
+        return words;
+    }
+
+
 
     @Override
     public String toString() {
--- src/MARCH14CreatingClasses/F13StringOperationTest.java
+++ src/MARCH14CreatingClasses/F13StringOperationTest.java
@@ -1,8 +1,13 @@
 package MARCH14CreatingClasses;
+import java.util.Arrays;
 
 public class F13StringOperationTest {
     public static void main(String[] args) {
-        F13StringOperation myStr = new F13StringOperation("Java");
+        F13StringOperation myStr = new F13StringOperation("java is super fun");
         System.out.println(myStr.returnReversed());
+        System.out.println(myStr.returnCapitalized());
+        System.out.println(Arrays.toString(myStr.returnString()));
+        System.out.println(Arrays.toString(myStr.returnSentence()));
+
     }
 }
